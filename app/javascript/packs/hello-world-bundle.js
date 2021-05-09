acdd6c8aaed5a1429e22d195c7c1147e9ba592ec
Kameron <kamahler@gmail.com>
2021-05-09 12:36:59 -0600

    change to App.js

--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,8 +1,8 @@
 import ReactOnRails from 'react-on-rails'
 
-import HelloWorld from '../bundles/components/HelloWorld'
+import App from '../bundles/components/App'
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
-  HelloWorld,
+  App,
 })
