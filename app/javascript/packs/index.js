bf1178f832819da1437cd6acc5733639e694723c
Kameron <kamahler@gmail.com>
2021-05-09 12:44:08 -0600

    change initial function and prop names

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -6,7 +6,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Hello = props => <div>Hello {props.name}!</div>
+const Hello = props => (
+  <>
+    <div>Hello {props.name}!</div>
+    <div>Hello {props.name}!</div>
+  </>
+)
 
 Hello.defaultProps = {
   name: 'David',
